9e3c6d06f638998103c0553cd451716ca59ba867
panterrich <panterrich@ispras.ru>
2024-07-04 01:33:06 +0300

    feature: add varjoin

--- sprint-1/varjoin/join.go
+++ sprint-1/varjoin/join.go
@@ -2,6 +2,37 @@
 
 package varjoin
 
+import "strings"
+
 func Join(sep string, args ...string) string {
-	return ""
+	switch argc := len(args); argc {
+	case 0:
+		return ""
+	case 1:
+		return args[0]
+	default:
+		sepLen := len(sep)
+		totalLen := (argc - 1) * sepLen
+
+		for _, s := range args {
+			totalLen += len(s)
+		}
+
+		var output strings.Builder
+		output.Grow(totalLen)
+		output.WriteString(args[0])
+
+		if sepLen > 0 {
+			for i := 1; i < argc; i++ {
+				output.WriteString(sep)
+				output.WriteString(args[i])
+			}
+		} else {
+			for i := 1; i < argc; i++ {
+				output.WriteString(args[i])
+			}
+		}
+
+		return output.String()
+	}
 }
