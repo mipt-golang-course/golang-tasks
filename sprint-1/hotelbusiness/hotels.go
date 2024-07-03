91cde2cf7667335ec09900d071da892cd5ee53fa
panterrich <panterrich@ispras.ru>
2024-07-04 02:06:35 +0300

    feature: add hotels

--- sprint-1/hotelbusiness/hotels.go
+++ sprint-1/hotelbusiness/hotels.go
@@ -2,6 +2,11 @@
 
 package hotelbusiness
 
+import (
+	"cmp"
+	"slices"
+)
+
 type Guest struct {
 	CheckInDate  int
 	CheckOutDate int
@@ -13,5 +18,31 @@ type Load struct {
 }
 
 func ComputeLoad(guests []Guest) []Load {
-	return []Load{}
+	if len(guests) == 0 {
+		return []Load{}
+	}
+
+	lastDate := slices.MaxFunc(guests, func(a, b Guest) int {
+		return cmp.Compare(a.CheckOutDate, b.CheckOutDate)
+	}).CheckOutDate
+
+	days := make([]Load, lastDate+1)
+
+	for _, g := range guests {
+		for i := g.CheckInDate; i < g.CheckOutDate; i++ {
+			days[i].GuestCount++
+		}
+	}
+
+	var load []Load
+	lastLoad := 0
+
+	for i, d := range days {
+		if lastLoad != d.GuestCount {
+			load = append(load, Load{StartDate: i, GuestCount: d.GuestCount})
+			lastLoad = d.GuestCount
+		}
+	}
+
+	return load
 }
