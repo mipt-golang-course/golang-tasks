d07e79ec7a25f5abbb4620229b10df6d1a62de1c
netzen86 <netzen0su@gmail.com>
2024-07-05 00:22:55 +0500

    varjoin complete

--- sprint-1/varjoin/join.go
+++ sprint-1/varjoin/join.go
@@ -3,5 +3,13 @@
 package varjoin
 
 func Join(sep string, args ...string) string {
-	return ""
+	var result string
+	if len(args) != 0 {
+		i := 0
+		for ; i < len(args)-1; i++ {
+			result = result + args[i] + sep
+		}
+		result = result + args[i]
+	}
+	return result
 }
