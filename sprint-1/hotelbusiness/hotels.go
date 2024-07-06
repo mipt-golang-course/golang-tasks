b04d34f1360441a3a9856eec4d8105e827a602a2
Kamil Kashapov <kashapov@ispras.ru>
2024-07-07 01:21:45 +0300

    hotel done

--- sprint-1/hotelbusiness/hotels.go
+++ sprint-1/hotelbusiness/hotels.go
@@ -13,5 +13,32 @@ type Load struct {
 }
 
 func ComputeLoad(guests []Guest) []Load {
-	return []Load{}
+	days := make(map[int]int)
+
+	maxDay := 0
+
+	for _, guest := range guests {
+		days[guest.CheckInDate] += 1
+		days[guest.CheckOutDate] -= 1
+		if guest.CheckInDate > maxDay {
+			maxDay = guest.CheckInDate
+		}
+
+		if guest.CheckOutDate > maxDay {
+			maxDay = guest.CheckOutDate
+		}
+	}
+
+	loads := make([]Load, 0, maxDay)
+
+	lastLoad := 0
+	for day := 0; day <= maxDay; day++ {
+		val, ok := days[day]
+		if ok && val != 0 {
+			lastLoad += val
+			loads = append(loads, Load{day, lastLoad})
+		}
+	}
+
+	return loads
 }
