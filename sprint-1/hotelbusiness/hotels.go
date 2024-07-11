2c8d47ff4673a950ee11aed32ef2d1e41f5c5a24
netzen86 <netzen0su@gmail.com>
2024-07-11 23:20:47 +0500

    hotelbusiness task complete

--- sprint-1/hotelbusiness/hotels.go
+++ sprint-1/hotelbusiness/hotels.go
@@ -2,6 +2,10 @@
 
 package hotelbusiness
 
+import (
+	"slices"
+)
+
 type Guest struct {
 	CheckInDate  int
 	CheckOutDate int
@@ -13,5 +17,36 @@ type Load struct {
 }
 
 func ComputeLoad(guests []Guest) []Load {
-	return []Load{}
+	var result []Load
+	var sliceDate []int
+	mapDate := make(map[int][]int)
+	var cnt int
+	var prevCnt int
+	for _, v := range guests {
+		if !slices.Contains(sliceDate, v.CheckInDate) {
+			sliceDate = append(sliceDate, v.CheckInDate)
+		}
+		if !slices.Contains(sliceDate, v.CheckOutDate) {
+			sliceDate = append(sliceDate, v.CheckOutDate)
+
+		}
+		mapDate[v.CheckInDate] = append(mapDate[v.CheckInDate], 1)
+		mapDate[v.CheckOutDate] = append(mapDate[v.CheckOutDate], -1)
+	}
+	slices.Sort(sliceDate)
+
+	for _, v := range sliceDate {
+		for _, gcv := range mapDate[v] {
+			if gcv > 0 {
+				cnt++
+			} else if gcv < 0 {
+				cnt--
+			}
+		}
+		if cnt != prevCnt {
+			result = append(result, Load{StartDate: v, GuestCount: cnt})
+		}
+		prevCnt = cnt
+	}
+	return result
 }
