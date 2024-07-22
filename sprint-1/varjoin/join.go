48b5f7ffe78fab38b1a1799b8bf7206df9168e1b
YasnoDelo <zielgerichtiet@mail.ru>
2024-07-22 18:39:15 +0300

    didn't use public func

--- sprint-1/varjoin/join.go
+++ sprint-1/varjoin/join.go
@@ -1,12 +1,18 @@
-//go:build !solution
-
 package varjoin
 
-import (
-	"strings"
-)
+// Join принимает разделитель и переменное число строк и возвращает одну строку-склейку всех аргументов через разделитель.
+func Join(sep string, parts ...string) string {
+	if len(parts) == 0 {
+		return ""
+	}
 
-// Join объединяет строки через разделитель
-func Join(sep string, elements ...string) string {
-	return strings.Join(elements, sep)
+	// Начинаем с первой строки, чтобы избежать начального разделителя
+	result := parts[0]
+
+	// Итерируем по остальным частям и добавляем их к результату с разделителем
+	for _, part := range parts[1:] {
+		result += sep + part
+	}
+
+	return result
 }
