baa8894ae27784ddd92288f3744b880f4cde5000
YasnoDelo <zielgerichtiet@mail.ru>
2024-08-11 23:56:38 +0300

    test in 3 sprint fixed

--- sprint-3/testequal/helper_test.go
+++ sprint-3/testequal/helper_test.go
@@ -1,19 +1,21 @@
-package testequal
+package testequal_test
 
 import (
 	"bytes"
+	"errors"
 	"os"
 	"os/exec"
 	"testing"
 
+	assertion "github.com/mipt-golang-course/golang-tasks/sprint-3/testequal"
 	"github.com/stretchr/testify/require"
 )
 
 func TestHelper(t *testing.T) {
 	if os.Getenv("FAIL_ASSERTIONS") == "1" {
-		AssertEqual(t, 1, 2, "%d must be equal to %d", 1, 2)
-		AssertNotEqual(t, 1, 1, "1 != 1")
-		RequireEqual(t, 1, 2)
+		assertion.AssertEqual(t, 1, 2, "%d must be equal to %d", 1, 2)
+		assertion.AssertNotEqual(t, 1, 1, "1 != 1")
+		assertion.RequireEqual(t, 1, 2)
 		return
 	}
 
@@ -23,10 +25,11 @@ func TestHelper(t *testing.T) {
 	cmd.Stdout = &buf
 
 	err := cmd.Run()
-	if e, ok := err.(*exec.ExitError); ok && !e.Success() {
-		require.Contains(t, buf.String(), "helper_test.go:14")
-		require.Contains(t, buf.String(), "helper_test.go:15")
+	var exitErr *exec.ExitError
+	if errors.As(err, &exitErr) && !exitErr.Success() {
 		require.Contains(t, buf.String(), "helper_test.go:16")
+		require.Contains(t, buf.String(), "helper_test.go:17")
+		require.Contains(t, buf.String(), "helper_test.go:18")
 		return
 	}
 	t.Fatalf("process ran with err %v, want exit status 1", err)
