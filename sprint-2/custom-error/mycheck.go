607839e5c6cab1d208d6c3ff3986a16d517933cf
Elena <elena.gibadullina@netcracker.com>
2024-09-12 02:03:26 +0300

    Sprint 2 commited

--- sprint-2/custom-error/mycheck.go
+++ sprint-2/custom-error/mycheck.go
@@ -1,8 +1,64 @@
-//go:build !solution
-
 package mycheck
 
+import (
+    "errors"
+    "strings"
+	"unicode"
+)
+
+// 0) Статическое указание ошибок на этапе инициализации
+var (
+	ErrNumbers = errors.New("found numbers")
+	ErrLength = errors.New("line is too long")
+	ErrSpaces = errors.New("no two spaces")
+)
+
+// 1) вставьте определение типа для []error
+type MyErr []error
+
+// 2) определите метод Error для вашего типа, который будет выводить все ошибки слайса
+
+func (myerrors MyErr) Error() string{
+	var result string
+
+	for iter, elem := range myerrors{
+		result = result + elem.Error()
+		if iter < len(myerrors)-1 {
+			result = result + ";"
+		}
+	}
+
+	return result
+}
+
+// 3) реализуйте функцию MyCheck
+
 func MyCheck(input string) error {
-	// TODO
+
+	errorsSlice := make(MyErr, 0)
+
+	var numbers = 0
+
+    for _, i := range input {
+		if unicode.IsDigit(i) {
+			numbers = 1
+			break
+		}
+    }
+
+	if numbers ==1 {
+		errorsSlice = append(errorsSlice, ErrNumbers)
+	}
+	if len(input) >= 20 {
+		errorsSlice = append(errorsSlice, ErrLength)
+	}
+	if strings.Count(input, " ") != 2 {
+		errorsSlice = append(errorsSlice, ErrSpaces)
+	}
+
+	if len(errorsSlice) > 0 {
+		return errorsSlice
+	}
+
 	return nil
 }
--- sprint-2/reflect-spell/reflect_spell.go
+++ sprint-2/reflect-spell/reflect_spell.go
@@ -1,5 +1,10 @@
 package reflect_spell
 
+import (
+	//"fmt"
+	"reflect"
+)
+
 type Spell interface {
 	// название заклинания
 	Name() string
@@ -21,7 +26,31 @@ func CastToAll(spell Spell, objects []interface{}) {
 }
 
 func CastTo(spell Spell, object interface{}) {
-	// реализуйте эту функцию.
+
+	if receiver, ok := object.(CastReceiver); ok {
+		receiver.ReceiveSpell(spell)
+		return
+	}
+
+	val := reflect.ValueOf(object)
+
+	if val.Kind() == reflect.Ptr {
+		val = val.Elem()
+	}
+
+	if val.Kind() != reflect.Struct {
+        return
+    }
+
+	field := val.FieldByName(spell.Char())
+
+	if field.IsValid() {
+		if field.CanSet() {
+			newValue := int(field.Int()) + spell.Value()
+			field.Set(reflect.ValueOf(newValue))
+		}
+	}
+
 }
 
 type spell struct {
@@ -73,3 +102,30 @@ type Orc struct {
 type Wall struct {
 	Durability int
 }
+
+/*func main() {
+
+    player := &Player{
+        name:   "Player_1",
+        health: 100,
+    }
+
+    enemies := []interface{}{
+        &Zombie{Health: 1000},
+        &Zombie{Health: 1000},
+        &Orc{Health: 500},
+        &Orc{Health: 500},
+        &Orc{Health: 500},
+        &Daemon{Health: 1000},
+        &Daemon{Health: 1000},
+        &Wall{Durability: 100},
+    }
+
+    CastToAll(newSpell("fire", "Health", -50), append(enemies, player))
+
+	fmt.Println(player)
+
+    CastToAll(newSpell("heal", "Health", 190), append(enemies, player))
+
+    fmt.Println(player)
+}*/
