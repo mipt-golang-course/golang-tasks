4f0813ae03e383ddf38ddcfadf339c1c71f31867
vincent_kir <vincent_kir@mail.ru>
2025-11-20 17:56:06 +0300

    add slove hostellbusiness after review

--- sprint-1/hotelbusiness/hotels.go
+++ sprint-1/hotelbusiness/hotels.go
@@ -3,7 +3,7 @@
 package hotelbusiness
 
 import (
-	"sort"
+	"slices"
 )
 
 type Guest struct {
@@ -17,79 +17,47 @@ type Load struct {
 }
 
 func ComputeLoad(guests []Guest) []Load {
-	loadMap := getLoadMap(&guests)
-	sortedKeysLoadMap := getSortedKeysLoadMap(&loadMap)
+	loadByDays := getLoadByDays(guests)
+	days := getDays(guests)
 
 	load := []Load{}
-	for _, day := range sortedKeysLoadMap {
-		load = append(load, Load{
-			StartDate:  day,
-			GuestCount: loadMap[day],
-		})
+
+	prevLoadDay := 0
+	for _, day := range days {
+		loadByDay := loadByDays[day] + prevLoadDay
+
+		if loadByDay != prevLoadDay {
+			load = append(load, Load{
+				StartDate:  day,
+				GuestCount: loadByDay,
+			})
+		}
+		prevLoadDay = loadByDay
 	}
 	return load
 }
 
-func getSortedKeysLoadMap(loadMap *map[int]int) []int {
-	keys := make([]int, 0, len(*loadMap))
-	for k := range *loadMap {
-		keys = append(keys, k)
-	}
-	sort.Ints(keys)
-	return keys
-}
-
-func getLoadMap(guests *[]Guest) map[int]int {
-
-	loadCheckIn := getLoadCheckIn(guests)
-	loadCheckOut := getLoadCheckOut(guests)
-	days := getDays(guests)
-
+func getLoadByDays(guests []Guest) map[int]int {
 	loadMap := make(map[int]int)
-
-	prevLoadDay := 0
-	for _, day := range days {
-		checkInByDay := loadCheckIn[day]
-		checkOutByDay := loadCheckOut[day]
-		loadByDay := checkInByDay - checkOutByDay + prevLoadDay
-
-		if loadByDay != prevLoadDay {
-			loadMap[day] = loadByDay
-		}
-
-		prevLoadDay = loadByDay
-
+	for _, guest := range guests {
+		loadMap[guest.CheckInDate]++
+		loadMap[guest.CheckOutDate]--
 	}
 	return loadMap
 }
 
-func getLoadCheckIn(guests *[]Guest) map[int]int {
-	loadMap := make(map[int]int)
-	for _, guest := range *guests {
-		loadMap[guest.CheckInDate] = loadMap[guest.CheckInDate] + 1
+func getDays(guests []Guest) []int {
+	setDays := make(map[int]struct{})
+	for _, guest := range guests {
+		setDays[guest.CheckInDate] = struct{}{}
+		setDays[guest.CheckOutDate] = struct{}{}
 	}
-	return loadMap
-}
 
-func getLoadCheckOut(guests *[]Guest) map[int]int {
-	loadMap := make(map[int]int)
-	for _, guest := range *guests {
-		loadMap[guest.CheckOutDate] = loadMap[guest.CheckOutDate] + 1
-	}
-	return loadMap
-}
-
-func getDays(guests *[]Guest) []int {
-	setDays := make(map[int]bool)
-	for _, guest := range *guests {
-		setDays[guest.CheckInDate] = true
-		setDays[guest.CheckOutDate] = true
-	}
 	days := make([]int, 0, len(setDays))
 
 	for day := range setDays {
 		days = append(days, day)
 	}
-	sort.Ints(days)
+	slices.Sort(days)
 	return days
 }
